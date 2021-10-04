bafda10492e3b3ba798a7f3727b5e487ccb61a35
Matheus Valle <matheusvalleguitar@gmail.com>
2021-10-04 16:01:16 -0300

    Ponto online

--- src/Home.js
+++ src/Home.js
@@ -129,7 +129,7 @@ export default function Home() {
             size="large"
             onClick={handlePonto}
           >
-            Entrada
+            Entrada ponto
           </Button>
           <Button onClick={getEntradas}>Get Dados</Button>
         </>
